2b4db90fdaf374346680e313827d89b9407e7d2d
Ken Ha <khanhha@nvg.vn>
2021-10-14 15:10:57 +0700

    add get github token

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
     const number: string = core.getInput('PullRequestNumber')
-    core.setOutput('PullRequestNumber: ', number)
+    const token: string = core.getInput('GITHUB_TOKEN')
+    const octokit = github.getOctokit(token)
+    const {data: pullRequest} = await octokit.pulls.get({
+      owner: 'octokit',
+      repo: 'rest.js',
+      pull_number: 123,
+      mediaType: {
+        format: 'diff'
+      }
+    })
+
+    core.setOutput('pullRequestNumber', number)
+    core.setOutput('pullRequestState', pullRequest.state)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
